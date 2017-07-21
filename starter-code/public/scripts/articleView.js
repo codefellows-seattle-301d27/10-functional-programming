5d730d22746438fc19029eab1f9f1d373cac3d06
la januari <lajanuari@gmail.com>
2017-07-21 16:53:44 -0700

    added Handlebars.compile function

--- starter-code/public/scripts/articleView.js
+++ starter-code/public/scripts/articleView.js
@@ -134,9 +134,12 @@ articleView.initIndexPage = function() {
 };
 
 articleView.initAdminPage = function() {
-  // TODO: Call the Handlebars `.compile` function, which will return a function for you to use where needed.
+  // DONE: Call the Handlebars `.compile` function, which will return a function for you to use where needed.
   // Make sure you assign the result of your Handlebars.compile call to a variable called "template", since
   // we are then calling "template" on line 117.
+
+  var templateString = $('#article-template').html();
+  var template = Handlebars.compile(templateString);
 
   // REVIEW: We use `forEach` here because we are relying on the side-effects of the callback function:
   // appending to the DOM.
