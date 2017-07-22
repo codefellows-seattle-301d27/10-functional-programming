4c5edbbc0191c198748d09ea2b6aa184ef57eb99
RecursiveIteration <James.C.Woodson@gmail.com>
2017-07-21 17:39:45 -0700

    used new global app object in the .html files

--- starter-code/public/scripts/article.js
+++ starter-code/public/scripts/article.js
@@ -38,7 +38,7 @@ var app = app || {};
   Article.loadAll = rows => {
     rows.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)));
 
-    // TODO: Refactor this forEach code, by using a `.map` call instead, since what we are trying to accomplish
+    // TODO: DONE Refactor this forEach code, by using a `.map` call instead, since what we are trying to accomplish
     // is the transformation of one collection into another. Remember that we can set variables equal to the result
     // of functions. So if we set a variable equal to the result of a .map, it will be our transformed array.
     // There is no need to push to anything.
@@ -48,7 +48,7 @@ var app = app || {};
     Article.all.push(new Article(ele));
   });
   */
-
+    Article.all.map(art => new Article(art));
   };
 
   Article.fetchAll = callback => {
@@ -61,9 +61,9 @@ var app = app || {};
     )
   };
 
-  // TODO: Chain together a `map` and a `reduce` call to get a rough count of all words in all articles.
+  // TODO: DONE Chain together a `map` and a `reduce` call to get a rough count of all words in all articles.
   Article.numWordsAll = () => {
-    return Article.all.map().reduce()
+    return Article.all.map(ele => ele.body.split(' ').length).reduce((total, len) => {total + len}, 0)
   };
 
   // TODO: Chain together a `map` and a `reduce` call to produce an array of unique author names. You will
