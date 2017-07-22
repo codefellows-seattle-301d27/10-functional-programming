f792e6921f262977d77bab0887d4b11f195f0319
ren burnett <elcreature@live.com>
2017-07-21 18:11:30 -0700

    wrapped the articleview in an IFEE, called Handlebars

--- starter-code/server.js
+++ starter-code/server.js
@@ -5,10 +5,10 @@ const pg = require('pg');
 const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 const app = express();
-// const conString = 'postgres://USERNAME:PASSWORD@HOST:PORT';
-const conString = `postgres://postgres:${process.env.PG_PASSWORD}@localhost:5432/kilovolt`; // TODO: DONE Don't forget to set your own conString
+const conString = 'postgres://localhost:5432/kilovolt';
+// const conString = `postgres://postgres:${process.env.PG_PASSWORD}@localhost:5432/kilovolt`; // TODO: DONE Don't forget to set your own conString
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', function(err) {
