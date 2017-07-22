b0b143f0086da5d04d74f714cca364c79887683c
Rami Bououni <rami.bououni@gmail.com>
2017-07-21 18:03:07 -0700

    finish function for number of words

--- starter-code/public/scripts/article.js
+++ starter-code/public/scripts/article.js
@@ -8,19 +8,19 @@ var app = app || {};
 // At the very end of the code, but still inside the IIFE, attach the 'Article' object to 'module'.
 // Where the IIFE is invoked, pass in the global 'app' object that is defined above.
 
-(function(module){
+(function(module) {
   function Article(rawDataObj) {
-  /* REVIEW: In lab 8, we explored a lot of new functionality going on here. Let's re-examine
-  the concept of context.
-  Normally, "this" inside of a constructor function refers to the newly instantiated object.
-  However, in the function we're passing to forEach, "this" would normally refer to "undefined"
-  in strict mode. As a result, we had to pass a second argument to forEach to make sure our "this"
-  was still referring to our instantiated object.
-  One of the primary purposes of lexical arrow functions, besides cleaning up syntax to use fewer
-  lines of code, is to also preserve context. That means that when you declare a function using
-  lexical arrows, "this" inside the function will still be the same "this" as it was outside
-  the function.
-  As a result, we no longer have to pass in the optional "this" argument to forEach!*/
+    /* REVIEW: In lab 8, we explored a lot of new functionality going on here. Let's re-examine
+    the concept of context.
+    Normally, "this" inside of a constructor function refers to the newly instantiated object.
+    However, in the function we're passing to forEach, "this" would normally refer to "undefined"
+    in strict mode. As a result, we had to pass a second argument to forEach to make sure our "this"
+    was still referring to our instantiated object.
+    One of the primary purposes of lexical arrow functions, besides cleaning up syntax to use fewer
+    lines of code, is to also preserve context. That means that when you declare a function using
+    lexical arrows, "this" inside the function will still be the same "this" as it was outside
+    the function.
+    As a result, we no longer have to pass in the optional "this" argument to forEach!*/
     Object.keys(rawDataObj).forEach(key => this[key] = rawDataObj[key]);
   }
 
@@ -29,7 +29,7 @@ var app = app || {};
   Article.prototype.toHtml = function() {
     var template = Handlebars.compile($('#article-template').text());
 
-    this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+    this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000);
     this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
     this.body = marked(this.body);
 
@@ -37,7 +37,7 @@ var app = app || {};
   };
 
   Article.loadAll = rows => {
-    rows.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)));
+    rows.sort((a, b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)));
 
     // DONE: Refactor this forEach code, by using a `.map` call instead, since what we are trying to accomplish
     // is the transformation of one collection into another. Remember that we can set variables equal to the result
@@ -49,7 +49,7 @@ var app = app || {};
     Article.all.push(new Article(ele));
   });
   */
-    rows.map(function(row){
+    rows.map(function(row) {
       return {
         title: row.title,
         category: row.category,
@@ -63,23 +63,32 @@ var app = app || {};
 
   Article.fetchAll = callback => {
     $.get('/articles')
-    .then(
-      results => {
-        Article.loadAll(results);
-        callback();
-      }
-    )
+      .then(
+        results => {
+          Article.loadAll(results);
+          callback();
+        }
+      )
   };
 
-  // TODO: Chain together a `map` and a `reduce` call to get a rough count of all words in all articles.
+  // Done: Chain together a `map` and a `reduce` call to get a rough count of all words in all articles.
+  // estimate: 20 min, actual: 10 min
   Article.numWordsAll = () => {
-    return Article.all.map().reduce()
+    return Article.all.map(function(article) {
+      var words = article.body.split(' ');
+      return words;
+    }).reduce(
+      function(words) {
+        return words.length;
+      })
   };
 
   // TODO: Chain together a `map` and a `reduce` call to produce an array of unique author names. You will
   // probably need to use the optional accumulator argument in your reduce call.
   Article.allAuthors = () => {
-    return Article.all.map().reduce();
+    return Article.all.map(fuction() {
+
+    }).reduce();
   };
 
   Article.numWordsByAuthor = () => {
@@ -97,47 +106,54 @@ var app = app || {};
 
   Article.truncateTable = callback => {
     $.ajax({
-      url: '/articles',
-      method: 'DELETE',
-    })
-    .then(console.log) // REVIEW: Check out this clean syntax for just passing 'assumed' data into a named function!
-                       // The reason we can do this has to do with the way Promise.prototype.then works. It's a little
-                       // outside the scope of 301 material, but feel free to research!
-    .then(callback);
+        url: '/articles',
+        method: 'DELETE',
+      })
+      .then(console.log) // REVIEW: Check out this clean syntax for just passing 'assumed' data into a named function!
+      // The reason we can do this has to do with the way Promise.prototype.then works. It's a little
+      // outside the scope of 301 material, but feel free to research!
+      .then(callback);
   };
 
   Article.prototype.insertRecord = function(callback) {
     // REVIEW: Why can't we use an arrow function here for .insertRecord()??
-    $.post('/articles', {author: this.author, authorUrl: this.authorUrl, body: this.body, category: this.category, publishedOn: this.publishedOn, title: this.title})
-    .then(console.log)
-    .then(callback);
-  };
-
-  Article.prototype.deleteRecord = function(callback) {
-    $.ajax({
-      url: `/articles/${this.article_id}`,
-      method: 'DELETE'
-    })
-    .then(console.log)
-    .then(callback);
-  };
-
-  Article.prototype.updateRecord = function(callback) {
-    $.ajax({
-      url: `/articles/${this.article_id}`,
-      method: 'PUT',
-      data: {
+    $.post('/articles', {
         author: this.author,
         authorUrl: this.authorUrl,
         body: this.body,
         category: this.category,
         publishedOn: this.publishedOn,
-        title: this.title,
-        author_id: this.author_id
-      }
-    })
-    .then(console.log)
-    .then(callback);
+        title: this.title
+      })
+      .then(console.log)
+      .then(callback);
+  };
+
+  Article.prototype.deleteRecord = function(callback) {
+    $.ajax({
+        url: `/articles/${this.article_id}`,
+        method: 'DELETE'
+      })
+      .then(console.log)
+      .then(callback);
+  };
+
+  Article.prototype.updateRecord = function(callback) {
+    $.ajax({
+        url: `/articles/${this.article_id}`,
+        method: 'PUT',
+        data: {
+          author: this.author,
+          authorUrl: this.authorUrl,
+          body: this.body,
+          category: this.category,
+          publishedOn: this.publishedOn,
+          title: this.title,
+          author_id: this.author_id
+        }
+      })
+      .then(console.log)
+      .then(callback);
   };
 
   module.Article = Article;
